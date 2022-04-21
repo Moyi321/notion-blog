dcf7adfde562d75b6ebdd73a2d418663c681fa81
Moyi321 <54176997+Moyi321@users.noreply.github.com>
2022-04-21 21:22:44 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '115e154abd0940f8922bcec9b86bd99e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Moyi-blog',
+  domain: 'https://www.notion.so/Moyi-115e154abd0940f8922bcec9b86bd99e',
+  author: 'moyi',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
